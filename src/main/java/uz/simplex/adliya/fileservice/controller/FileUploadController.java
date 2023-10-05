9c484677474986945f81ad711cf376964c9c10f5
Sardor Ubaydullayev <sardorubaydullayev20@gmail.com>
2023-10-05 22:35:56 +0500

    fix bug

--- src/main/java/uz/simplex/adliya/fileservice/controller/FileUploadController.java
+++ src/main/java/uz/simplex/adliya/fileservice/controller/FileUploadController.java
@@ -9,8 +9,8 @@ import org.springframework.web.multipart.MultipartFile;
 import uz.simplex.adliya.fileservice.dto.FileUploadResponse;
 import uz.simplex.adliya.fileservice.service.FileUploadService;
 
-@RequestMapping
-@RestController("/api/v1/upload")
+@RequestMapping("/api/v1/upload")
+@RestController
 public class FileUploadController {
 
     private final FileUploadService fileUploadService;
