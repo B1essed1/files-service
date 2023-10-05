1d7c15a762cc6ea47234a32b21d80d93359d9ff4
Abdulla Axmadaliyev <aa1549aa@gmail.com>
2023-10-05 22:41:52 +0500

    hi res

--- src/main/java/uz/simplex/adliya/fileservice/controller/FileUploadController.java
+++ src/main/java/uz/simplex/adliya/fileservice/controller/FileUploadController.java
@@ -1,16 +1,14 @@
 package uz.simplex.adliya.fileservice.controller;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import uz.simplex.adliya.fileservice.dto.FileUploadResponse;
+import uz.simplex.adliya.fileservice.dto.SimpleResponse;
 import uz.simplex.adliya.fileservice.service.FileUploadService;
 
-@RequestMapping
-@RestController("/api/v1/upload")
+@RequestMapping("/api/file/v1/upload")
+@RestController
 public class FileUploadController {
 
     private final FileUploadService fileUploadService;
@@ -22,6 +20,11 @@ public class FileUploadController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public FileUploadResponse upload(@RequestPart MultipartFile multipartFile) {
         return fileUploadService.upload(multipartFile);
+    }
+
+    @GetMapping("hi")
+    public SimpleResponse hi() {
+        return new SimpleResponse();
     }
 
 
---
+++ src/main/java/uz/simplex/adliya/fileservice/dto/SimpleResponse.java
@@ -0,0 +1,11 @@
+package uz.simplex.adliya.fileservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SimpleResponse {
+
+    private String message = "hi from file service";
+}
