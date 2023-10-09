69a36fb86bd54a116fd10d10709f6cbdf4974a05
Sardor Ubaydullayev <sardorubaydullayev20@gmail.com>
2023-10-09 10:09:30 +0500

    edit eurica url and database url

--- src/test/java/uz/simplex/adliya/fileservice/FileEntityServiceApplicationTests.java
+++ src/test/java/uz/simplex/adliya/fileservice/FileEntityServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package uz.simplex.adliya.fileservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FileEntityServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package uz.simplex.adliya.fileservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FileEntityServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
