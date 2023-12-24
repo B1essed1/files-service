4aa6c26d0c95eb4dc0ae086794ea93c31b81229c
B1essed1 <mrshakh001@gmail.com>
2023-12-24 18:42:17 +0500

    checkpoint

--- src/main/java/uz/simplex/adliya/fileservice/utils/CONSTANTS.java
+++ src/main/java/uz/simplex/adliya/fileservice/utils/CONSTANTS.java
@@ -2,4 +2,5 @@ package uz.simplex.adliya.fileservice.utils;
 
 public  class CONSTANTS {
     public  static final String  BASE_URL = "http://165.232.122.8:50000";
+    public  static final String  BASE_DIR = "/upload";
 }
