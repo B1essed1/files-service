f7e68f5f38bff6a3f528cc0bff08a48423fe4a21
B1essed1 <mrshakh001@gmail.com>
2024-02-08 11:34:31 +0500

    fix

--- src/main/java/uz/simplex/adliya/fileservice/FileServiceApplication.java
+++ src/main/java/uz/simplex/adliya/fileservice/FileServiceApplication.java
@@ -1,13 +1,18 @@
 package uz.simplex.adliya.fileservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(scanBasePackages = "uz.simplex.adliya")
+@SpringBootApplication(scanBasePackages = "uz.simplex.adliya",  exclude = {
+        MultipartAutoConfiguration.class } )
 @EnableDiscoveryClient
 @EnableAsync
+// excluded so that the application uses commons-fileupload instead of Servlet 3 Multipart support
+
 public class FileServiceApplication {
 
     public static void main(String[] args) {
